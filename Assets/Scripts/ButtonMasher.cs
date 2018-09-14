f0bc64f30b4a7f35501333362af6337b9fd30fdb
Dylan Christie <43067359+Argedco@users.noreply.github.com>
2018-09-14 13:57:24 +0100

    Added Music and Button Click SFX
    Added Music for the background, and coded a button to make a clicking sound when mouse clicked down.

--- Assets/Scripts/ButtonMasher.cs
+++ Assets/Scripts/ButtonMasher.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class ButtonMasher : MonoBehaviour {
 
+    // Data for our class
+    public AudioSource clickSound; // Button Clicking Sound
+
 	// Use this for initialization
 	void Start () {
         Debug.Log("Start Method Called.");
 
-	}
+	} // End of Start()
 	
 	// Update is called once per frame
 	void Update () {
         Debug.Log("Update Method Triggered.");
-	}
-}
+	} // End of Update()
+
+    //Responds to to even from Unity, that the object has been clicked.
+    private void OnMouseDown() {
+        Debug.Log("The mouse has clicked on the object. OnMouseDown triggered.");
+        clickSound.Play();
+    } // End of OnMouseDown.
+} // End of ButtonMasher Class
